3c903def01843112ec0b0e7f7f8e3de96c10a263
Sergey Shapiro <syrill.snir@gmail.com>
2023-12-14 20:57:22 +0100

    2023 08 09 добавил комментарии в parser.py

--- tmp.py
+++ tmp.py
@@ -1,1 +1,5 @@
+length = float(input('Введите длину прямоугольника, см: '))
+width = float(input('Введите ширину прямоугольника, см: '))
+perimetr = (length + width) * 2
+print(f'Периметр прямоугольника равен: {perimetr} см')
 
